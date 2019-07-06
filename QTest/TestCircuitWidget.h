c694218666e2d70426c8503e43171214a187a54e
cipher <alessio.bonacchi@stud.unifi.it>
2019-07-06 12:45:36 +0200

    QTest setup FirstTry

---
+++ QTest/TestCircuitWidget.h
@@ -0,0 +1,21 @@
+//
+// Created by Sierra on 2019-07-01.
+//
+
+#ifndef FIRSTSIMULATORTEST_TESTCIRCUITWIDGET_H
+#define FIRSTSIMULATORTEST_TESTCIRCUITWIDGET_H
+
+#include <QtTest/QtTest>
+#include <iostream>
+
+
+class TestCircuitWidget: public QObject {
+Q_OBJECT
+    void testAddItem();
+    void removeRandomComponent();
+    void createAndRemoveOrsaMaggiore(); //TODO
+};
+
+
+
+#endif //FIRSTSIMULATORTEST_TESTCIRCUITWIDGET_H
