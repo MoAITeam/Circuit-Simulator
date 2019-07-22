b3f1efe31798cec6899948c1a68ca55c07d27e7c
marco <marco.guerra@stud.unifi.it>
2019-07-22 12:55:16 -0700

    Revert "cmake trial"
    This reverts commit 9a36e4e7

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -105,7 +105,7 @@ void MainWindow::createMenus() {
     //fileMenu->addAction(exitAction);
 
     itemMenu= menuBar()->addMenu(tr("&Item"));
-    itemMenu->addAction(deleteAction);
+    //itemMenu->addAction(deleteAction);
 }
 
 void MainWindow::keyPressEvent(QKeyEvent *event) {
