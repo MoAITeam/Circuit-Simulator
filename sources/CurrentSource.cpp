cee90bacdfb49de8220ca31d14bb8f440e931a18
marco <marco.guerra@stud.unifi.it>
2019-08-13 11:02:49 -0700

    Fix graphical bug

--- sources/CurrentSource.cpp
+++ sources/CurrentSource.cpp
@@ -8,7 +8,7 @@
 void CurrentSource::paint(QPainter *painter, const QStyleOptionGraphicsItem *qg, QWidget *qw) {
 
     painter->setPen(QPen(Qt::blue, 2, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
-    Component::paint(painter,qg,qw);
+    ActiveComponent::paint(painter,qg,qw);
 }
 
 CurrentSource::CurrentSource(float value):ActiveComponent(0,1,value) {
--- sources/Voltmeter.cpp
+++ sources/Voltmeter.cpp
@@ -8,9 +8,10 @@
 
 void Voltmeter::paint(QPainter *painter, const QStyleOptionGraphicsItem *qg, QWidget *qw) {
 
+    painter->save();
     painter->setPen(QPen(Qt::blue, 2, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
     Component::paint(painter,qg,qw);
-    painter->resetTransform();
+    painter->restore();
     QPointF center((nodes.first->x()+nodes.second->x())/2, (nodes.first->y()+nodes.second->y())/2);
     painter->translate(center);
     painter->setPen(QPen(Qt::black, 2, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
